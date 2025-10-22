91b8cd670c1971d98590778fc852e9458699c871
afrakiw-design <afrakiw@gmail.com>
2025-10-22 07:17:39 +0700

    membuat website khusus

---
+++ data.js
@@ -0,0 +1,7 @@
+const namaSaya = "Afra Panji" ;
+let Usia = 15 ;
+console.log(`Nama saya adalah ${namaSaya} dan usia saya ${Usia} tahun`) ;
+
+function generateBiodata() {
+    console.log(`Nama saya adalah ${namaSaya} dan usia saya ${Usia} tahun`) ;
+}
